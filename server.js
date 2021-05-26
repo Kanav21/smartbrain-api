0ba51d60551e7639b11ee97f550d03ef494d79a0
Kanav <kanavjain21@gmail.com>
2021-05-26 18:52:55 +0530

    changing database name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'postgresql',
+    user : 'postgres',
     password : '1234',
     database : 'smart-brain'
   }
